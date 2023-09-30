9166fa187d1b395029f8afbde9c5a86a53936910
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-09-30 20:20:14 +0530

    Added login api and able to generate JWT token

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const { use } = require('../routes/user');
+const jwt = require('jsonwebtoken');
+const { options } = require('../routes/user');
+require("dotenv").config();
+
 
 // signup route handler
 exports.signup = async(req, res) => {
@@ -44,5 +47,75 @@ exports.signup = async(req, res) => {
             success:false,
             message: 'User cannot be registered, Please try again later.'
         })
+    }
+}
+
+// login
+exports.login = async(req, res) => {
+    try {
+        // data fetch
+        const {email, password} = req.body;
+        // validation on email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Plese fill all the details carefully"
+            });
+        }
+        // check user is available or not
+        let user = await User.findOne({email});
+        // if user is not registered
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User is not registered"
+            });
+        }
+        // create payload for jwt
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role
+        }
+        // verify password & generate a JWT token
+        if(await bcrypt.compare(password, user.password)){
+            // password match
+            // create jwt token
+            let token = jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn:"2h"
+            });
+            // first convert user to object then add token
+            user = user.toObject();
+            user.token = token;
+            // remove password for securtiy purpose and its remove from user object not database
+            user.password = undefined;
+
+            // creating cookie
+            const options = {
+                expires: new Date(Date.now() + 3 *24 * 60 * 60 * 1000),
+                httpOnly:true // means not access in client side
+
+            }
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token: token,
+                user_details: user,
+                message:"User Logged in successfully"
+
+            });
+        } else{
+            // password not match
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect Password"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Login Failure",
+            err: error
+        });
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {signup} = require('../Controller/Auth');
+const {signup, login} = require('../Controller/Auth');
 router.post('/signup', signup);
-
+router.post('/login', login);
 module.exports = router;
